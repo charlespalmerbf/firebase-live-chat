bffe03daaf737b868b2a6ed9940b45e2705a88ef
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2024-06-04 16:29:50 +0100

    feat: adding env plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import EnvironmentPlugin from "vite-plugin-environment";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        EnvironmentPlugin([
+            "FIREBASE_API_KEY",
+            "FIREBASE_AUTH_DOMAIN",
+            "FIREBASE_PROJECT_ID",
+            "FIREBASE_STORAGE_BUCKET",
+            "FIREBASE_MESSAGING_SENDER_ID",
+            "FIREBASE_APP_ID",
+            "FIREBASE_MEASUREMENT_ID",
+        ]),
+    ],
+    resolve: {
+        alias: {
+            src: "/src",
+        },
+    },
+});
